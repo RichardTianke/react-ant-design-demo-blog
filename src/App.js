e3fd30436fa12cf9bbcabc360f0d85b203fc32db
RichardTian <richard@crazybaby.com>
2017-07-20 11:38:16 +0800

    [+] config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { DatePicker, message} from 'antd';
+import Button from 'antd/lib/button';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            date: '',
+        };
+    }
+
+    handleChange(date){
+        message.info('您选择的日期是: ' + date.toString());
+        this.setState({
+            date
+        });
+    }
+
+    render(){
+        return (
+            <div style={{ with: 400, margin: '100px auto'}}>
+
+                <DatePicker onChange={value => this.handleChange(value)}/>
+                <div style={{ marginTop: 20, marginBottom: 20}}>当前日期: {this.state.date.toString()}</div>
+
+
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/routes/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Products extends React.Component{
+    render(){
+        return (
+            <h2>List of products</h2>
+        );
+    }
+}
+
+export default Products;
